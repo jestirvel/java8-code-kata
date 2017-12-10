142342b6ae8ba7954f469f20201094b8a5f00b6b
jestirvel <jtirado@gmail.com>
2017-12-10 16:22:07 +0100

    completed exercises 6-8 of stream-api

--- stream-api/src/test/java/stream/api/Exercise8Test.java
+++ stream-api/src/test/java/stream/api/Exercise8Test.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.hasItems;
@@ -19,36 +22,43 @@ import static org.junit.Assert.*;
 
 public class Exercise8Test extends ClassicOnlineStore {
 
-    @Difficult @Test
-    public void itemsNotOnSale() {
-        Stream<Customer> customerStream = this.mall.getCustomerList().stream();
-        Stream<Shop> shopStream = this.mall.getShopList().stream();
+	@Difficult
+	@Test
+	public void itemsNotOnSale() {
+		Stream<Customer> customerStream = this.mall.getCustomerList().stream();
+		Stream<Shop> shopStream = this.mall.getShopList().stream();
 
-        /**
-         * Create a set of item names that are in {@link Customer.wantToBuy} but not on sale in any shop.
-         */
-        List<String> itemListOnSale = null;
-        Set<String> itemSetNotOnSale = null;
+		/**
+		 * Create a set of item names that are in {@link Customer.wantToBuy} but not on
+		 * sale in any shop.
+		 */
+		List<String> itemListOnSale = shopStream.flatMap(s -> s.getItemList().stream()).map(Item::getName).distinct()
+				.collect(Collectors.toList());
+		Set<String> itemSetNotOnSale = customerStream.flatMap(c -> c.getWantToBuy().stream()).map(Item::getName)
+				.filter(n -> itemListOnSale.stream().noneMatch(i -> n.equals(i))).collect(Collectors.toSet());
 
-        assertThat(itemSetNotOnSale, hasSize(3));
-        assertThat(itemSetNotOnSale, hasItems("bag", "pants", "coat"));
-    }
+		assertThat(itemSetNotOnSale, hasSize(3));
+		assertThat(itemSetNotOnSale, hasItems("bag", "pants", "coat"));
+	}
 
-    @Difficult @Test
-    public void havingEnoughMoney() {
-        Stream<Customer> customerStream = this.mall.getCustomerList().stream();
-        Stream<Shop> shopStream = this.mall.getShopList().stream();
+	@Difficult
+	@Test
+	public void havingEnoughMoney() {
+		Stream<Customer> customerStream = this.mall.getCustomerList().stream();
+		Stream<Shop> shopStream = this.mall.getShopList().stream();
 
-        /**
-         * Create a customer's name list including who are having enough money to buy all items they want which is on sale.
-         * Items that are not on sale can be counted as 0 money cost.
-         * If there is several same items with different prices, customer can choose the cheapest one.
-         */
-        List<Item> onSale = null;
-        Predicate<Customer> havingEnoughMoney = null;
-        List<String> customerNameList = null;
+		/**
+		 * Create a customer's name list including who are having enough money to buy
+		 * all items they want which is on sale. Items that are not on sale can be
+		 * counted as 0 money cost. If there is several same items with different
+		 * prices, customer can choose the cheapest one.
+		 */
+		List<Item> onSale = shopStream.flatMap(s -> s.getItemList().stream()).distinct().collect(Collectors.toList());
+		ToIntFunction<Item> mapper = item -> onSale.stream().filter(s -> s.getName().equals(item.getName())).sorted((a, b) -> a.getPrice() - b.getPrice()).findFirst().map(Item::getPrice).orElse(0);
+		Predicate<Customer> havingEnoughMoney = c -> c.getBudget() >= c.getWantToBuy().stream().mapToInt(mapper).sum();
+		List<String> customerNameList = customerStream.filter(havingEnoughMoney).map(Customer::getName).collect(Collectors.toList());
 
-        assertThat(customerNameList, hasSize(7));
-        assertThat(customerNameList, hasItems("Joe", "Patrick", "Chris", "Kathy", "Alice", "Andrew", "Amy"));
-    }
+		assertThat(customerNameList, hasSize(7));
+		assertThat(customerNameList, hasItems("Joe", "Patrick", "Chris", "Kathy", "Alice", "Andrew", "Amy"));
+	}
 }
